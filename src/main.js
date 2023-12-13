5246011502e52d2580a594eddaa437b4371e6c01
gongahkia <angryapplegravy@gmail.com>
2023-12-13 12:21:47 +0800

    dine

--- src/main.js
+++ src/main.js
@@ -1,23 +1,45 @@
 alert("tikrit begins");
 
 // ---------- LEGEND ----------
+
 // . => air block
-// P => player
-// B => block
-// E => enemy
-// I => item 
+// @ => player (white)
+// X => solid wall indoor outdoor (grey)
+// [#, ¥] => trees (dark green)
+// [~, ] => water
+// = => bridge over gap or spikes or water (brown)
+// ^ => spikes
+// [",',`] => grass (mixes of bright and dark green)
+// [+, -] => open door, closed door
+// [Ø, Ö]  => locked door, cave entrance
+// & => health pickup
+// ! => weapon pickup
+// % => item pickup
+// ? => mystery chest
+// ê => boss
+// M => enemy mob
+// R => enemy rampager
+// S => enemy sniper
+// B => enemy bomber
+
+// !! DONT RENDER AIR BLOCK IN ACTUAL RENDER ENGINE !!
 
 // ---------- CANVAS ----------
 
 // instantiating canvas
 const c1 = document.getElementById("c1");
+
+// dynamically resizes canvas
+/* 
 c1.width = window.innerWidth;
 c1.height = window.innerHeight;
+*/
+
+c1.width = 700;
+c1.height = 700;
+
 console.log(c1);
 const c = c1.getContext("2d");
-
-// half-screen size => 798 width, 789 height
-// full-screen size => 1600 width,  789 height
 
 // ---------- PREP WORK ----------
 
@@ -28,102 +50,9 @@ const keys = {
   d: false,
 };
 
+// FUA add each rendering model to a dictionary
+
 // ----- WORLD OBJECTS -----
-
-const world = {
-
-  ui: {
-
-    char: {
-      // rendering defaults
-      spriteSrc: "", // FUA add here
-      srcX: 0,
-      srcY: 0,
-      srcWidth: 32, // FUA EDIT ACCORDINGLY
-      srcHeight: 32,
-      destWidth: 32,
-      destHeight: 32,
-
-      // character information
-      destX: 0, // FUA EDIT ACCORDINGLY, probably need a function to generate coordinates iteratively
-      destY: 0,
-    }
-
-  },
-
-  object: {
-
-    block: {
-      // rendering defaults
-      spriteSrc: "", // FUA add here
-      srcX: 0,
-      srcY: 0,
-      srcWidth: 32, // FUA EDIT ACCORDINGLY
-      srcHeight: 32,
-      destWidth: 32,
-      destHeight: 32,
-
-      // coord array 2 be rendered
-      coord: [
-        {destX:0, destY:5},
-        {destX:2, destY:10} // FUA ADD STUFF HERE but make this update dynamically by reading a text file
-      ]
-
-    },
-
-    item : {
-
-      healthPickup: {
-        // rendering defaults
-        spriteSrc: "", // FUA add here
-        srcX: 0,
-        srcY: 0,
-        srcWidth: 32, // FUA EDIT ACCORDINGLY
-        srcHeight: 32,
-        destWidth: 32,
-        destHeight: 32,
-
-        // coord array 2 be rendered
-        coord: [
-          // can be empty if no items
-        ]
-      },
-
-      weaponPickup: {
-        // rendering defaults
-        spriteSrc: "", // FUA add here
-        srcX: 0,
-        srcY: 0,
-        srcWidth: 32, // FUA EDIT ACCORDINGLY
-        srcHeight: 32,
-        destWidth: 32,
-        destHeight: 32,
-
-        // coord array 2 be rendered
-        coord: [
-          // can be empty if no items
-        ]
-      },
-
-      speedPickup: {
-        // rendering defaults
-        spriteSrc: "", // FUA add here
-        srcX: 0,
-        srcY: 0,
-        srcWidth: 32, // FUA EDIT ACCORDINGLY
-        srcHeight: 32,
-        destWidth: 32,
-        destHeight: 32,
-
-        // coord array 2 be rendered
-        coord: [
-          // can be empty if no items
-        ]
-      },
-
-    }
-
-  },
 
 // ----- ENTITIES ----- 
 
@@ -132,3 +61,5 @@ const world = {
 // ---------- USER INPUT ----------
 
 // ---------- EVENT LOOP ----------
+c.font = "20px 'Comic Sans MS', cursive";
+c.fillText("@",10,30);
